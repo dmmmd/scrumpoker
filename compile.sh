0b6dc9a6cbff55968d91e0d27352004fc7707c26
Evgenii Savintcev <me@dmmd.me>
2019-04-06 20:14:06 +0300

    Minor cleanup

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker build -t scrumpoker-compiler -f docker/server/Dockerfile.compiler .
-docker run --detach --name=scrumpoker-compiler --publish 80:80 scrumpoker-compiler
+docker run --detach --name=scrumpoker-compiler scrumpoker-compiler
 docker cp scrumpoker-compiler:/scrumpoker/scrumpoker docker/server/scrumpoker
 docker container stop scrumpoker-compiler
 docker container rm scrumpoker-compiler
